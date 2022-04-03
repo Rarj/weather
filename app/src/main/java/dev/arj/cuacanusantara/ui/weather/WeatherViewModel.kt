b2455becc1d451a3b9245fb938ac8e180fce64a8
Rio Arjuna <ri0arjuna@programmer.net>
2022-04-03 21:06:13 +0700

    add view model to present data into UI layer

---
+++ app/src/main/java/dev/arj/cuacanusantara/ui/weather/WeatherViewModel.kt
@@ -0,0 +1,34 @@
+package dev.arj.cuacanusantara.ui.weather
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.arj.cuacanusantara.data.weather.model.WeatherResponse
+import dev.arj.cuacanusantara.domain.weather.usecase.WeatherUseCase
+import dev.arj.cuacanusantara.network.ViewState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class WeatherViewModel(private val useCase: WeatherUseCase) : ViewModel() {
+
+    private var _weatherState = MutableStateFlow(ViewState<WeatherResponse>())
+    val weatherState: StateFlow<ViewState<WeatherResponse>>
+        get() = _weatherState
+
+    fun fetchCurrentWeather(latitude: String, longitude: String) {
+        _weatherState.value.loading = true
+
+        viewModelScope.launch {
+            useCase.fetchCurrentWeather(latitude, longitude)
+                .catch { throwable ->
+                    _weatherState.value.errorMessage = throwable.message
+                }
+                .collect { response ->
+                    _weatherState.value.success = response
+                }
+        }
+    }
+
+}
