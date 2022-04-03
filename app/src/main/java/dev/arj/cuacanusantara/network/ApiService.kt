1c82b7c07e27fc17a1761b71fe00b35db688f495
Rio Arjuna <ri0arjuna@programmer.net>
2022-04-03 07:33:36 +0700

    add service to get current weather

--- app/src/main/java/dev/arj/cuacanusantara/network/ApiService.kt
+++ app/src/main/java/dev/arj/cuacanusantara/network/ApiService.kt
@@ -1,5 +1,16 @@
 package dev.arj.cuacanusantara.network
 
+import dev.arj.cuacanusantara.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("weather")
+    suspend fun getCurrentWeather(
+        @Query("lat") latitude: Long,
+        @Query("long") longitude: Long,
+        @Query("appid") api_key: String = BuildConfig.API_KEY
+    )
 
 }
