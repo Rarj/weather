0d99cec6ebd650514ff3259bf1666b2e12914500
Rio Arjuna <ri0arjuna@programmer.net>
2022-04-03 21:06:50 +0700

    fixing wrong query name for longitude

--- app/src/main/java/dev/arj/cuacanusantara/network/ApiService.kt
+++ app/src/main/java/dev/arj/cuacanusantara/network/ApiService.kt
@@ -1,6 +1,7 @@
 package dev.arj.cuacanusantara.network
 
 import dev.arj.cuacanusantara.BuildConfig
+import dev.arj.cuacanusantara.data.weather.model.WeatherResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,9 +9,9 @@ interface ApiService {
 
     @GET("weather")
     suspend fun getCurrentWeather(
-        @Query("lat") latitude: Long,
-        @Query("long") longitude: Long,
+        @Query("lat") latitude: String,
+        @Query("lon") longitude: String,
         @Query("appid") api_key: String = BuildConfig.API_KEY
-    )
+    ): WeatherResponse
 
 }
