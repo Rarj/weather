dc18c4de9d18cc1ef2f1e947439730256d623173
Rio Arjuna <ri0arjuna@programmer.net>
2022-04-16 19:50:19 +0700

    remove full screen

--- app/src/main/java/dev/arj/cuacanusantara/MainActivity.kt
+++ app/src/main/java/dev/arj/cuacanusantara/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupFullScreen()
         setupDataBinding()
     }
 
